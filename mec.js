9ff6313e06d96090b573e875738a5dba4e413651
Manohar R <142599616+Manoharr108@users.noreply.github.com>
2023-12-24 17:13:40 +0530

    Update mec.js

--- mec.js
+++ mec.js
@@ -4,11 +4,16 @@
 
 let comp = 0;
 let play = 0;
+let count = 0;
+count = Number.parseInt(count);
 let p_s = document.getElementById("p_score"). textContent = comp;
 let c_s = document.getElementById("c_score"). textContent = play;
+p_s = Number.parseInt(p_s);
+c_s = Number.parseInt(c_s);
+document.getElementsByClassName("progress-bar")[0].setAttribute("style","width:"+(p_s/count)*100+"%")
 
 function useRock(){
-
+    document.getElementsByClassName("progress-bar")[0].setAttribute("style","width: 0px")
     let array = [1, 2, 3];
     let res = Math.floor(Math.random() * 3 );
     res = Number.parseInt(res);
@@ -23,28 +28,32 @@ function useRock(){
 
 
     if(array[res] == 1){
-        alert("It's a Tie!!");
+        alert("It's a Tie!! Computer choose Rock too");
         comp++;
         play++;
     }
     else if(array[res] == 2){
-        alert("You lost!");
+        alert("You lost! Computer choose paper");
         comp++;
     }
     else{
-        alert("You Won!!");
+        alert("You Won!! Computer choose scissors");
         play++;
     }
-p_s = document.getElementById("p_score"). textContent = comp;
-c_s = document.getElementById("c_score"). textContent = play;
+    p_s = document.getElementById("p_score"). textContent = play;
+    c_s = document.getElementById("c_score"). textContent = comp;
+    count++;
+    console.log("Win ="+(p_s/count)*100)
+    document.getElementsByClassName("progress-bar")[0].setAttribute("style","width:"+(p_s/count)*100+"%")
+    let won = Math.round((p_s/count)*100)
+    ultra.innerHTML = won + " %"
 }
 
 function usePaper(){
+    document.getElementsByClassName("progress-bar")[0].setAttribute("style","width: 0px")
     let array = [1, 2, 3];
     let res = Math.floor(Math.random() * 3 );
     res = Number.parseInt(res);
-
-    console.log(array[res]);
     if(array[res] == 1)
     console.log("Rock")
     else if(array[res] == 2)
@@ -66,12 +75,17 @@ function usePaper(){
         alert("You lost!");
         comp++;
     }
-    p_s = document.getElementById("p_score"). textContent = comp;
-c_s = document.getElementById("c_score"). textContent = play;
-
+    p_s = document.getElementById("p_score"). textContent = play;
+    c_s = document.getElementById("c_score"). textContent = comp;
+    count++;
+    document.getElementsByClassName("progress-bar")[0].setAttribute("style","width:"+(p_s/count)*100+"%")
+    let won = Math.round((p_s/count)*100)
+    ultra.innerHTML = won + " %"
 }
 
 function useScissors(){
+    document.getElementsByClassName("progress-bar")[0].setAttribute("style","width: 0px")
+
     let array = [1, 2, 3];
     let res = Math.floor(Math.random() * 3 );
     res = Number.parseInt(res);
@@ -98,15 +112,22 @@ function useScissors(){
         play++;
         comp++;
     }
-    p_s = document.getElementById("p_score"). textContent = comp;
-c_s = document.getElementById("c_score"). textContent = play;
+    p_s = document.getElementById("p_score"). textContent = play;
+    c_s = document.getElementById("c_score"). textContent = comp;
+    count++;
+    console.log("Win ="+(p_s/count)*100)
+    document.getElementsByClassName("progress-bar")[0].setAttribute("style","width:"+(p_s/count)*100+"%")
+    let won = Math.round((p_s/count)*100)
+    ultra.innerHTML = won + " %"
 }
 
 function dlte(){
     play=0;
     comp=0;
+    count = 0;
     p_s = document.getElementById("p_score"). textContent = 0;
     c_s = document.getElementById("c_score"). textContent = 0;
+    document.getElementsByClassName("progress-bar")[0].setAttribute("style","width:0px")
 }
 
 
